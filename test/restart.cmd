52f5acceb2258eba9c722d490330d5797a23dc0f
rockyb <rockyb>
2006-01-04 07:45:11 +0000

    dbg-cmds.inc: starting with bash 3.1 input file descriptors must be less than 9.
    dbg-io.inc: fix off by one counting of file line numbers.
    
    test/* adjust for revised output.

--- test/restart.cmd
+++ test/restart.cmd
@@ -1,4 +1,4 @@
-# $Id: restart.cmd,v 1.1 2006/01/02 23:34:26 rockyb Exp $
+# $Id: restart.cmd,v 1.2 2006/01/04 07:45:12 rockyb Exp $
 # Test Restart command
 list
 step
@@ -6,4 +6,5 @@ step
 break 7
 restart -B -q -L .. -x restart2.cmd restartbug.sh
 # We never get here
+print You should not see this.
 quit 
