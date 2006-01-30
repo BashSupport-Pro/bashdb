afbf3f3620ccd338569bebd6856c40ae20ddc8b5
rockyb <rockyb>
2006-01-30 01:50:56 +0000

    Add options processing tests.

---
+++ test/continue.cmd
@@ -0,0 +1,4 @@
+# $Id: continue.cmd,v 1.1 2006/01/30 01:50:56 rockyb Exp $
+# continue and quit command.
+continue
+quit
---
+++ test/quit.cmd
@@ -0,0 +1,3 @@
+# $Id: quit.cmd,v 1.1 2006/01/30 01:50:56 rockyb Exp $
+# Just a simple quit command.
+quit
