b955e1471f5b88e7bb37990baa8a95f953ba756a
rockyb <rockyb>
2006-03-19 11:09:40 +0000

    Add more completion tests

--- test/complete.cmd
+++ test/complete.cmd
@@ -1,6 +1,13 @@
 # Test of debugger complete command
-# $Id: complete.cmd,v 1.2 2006/03/15 12:54:40 rockyb Exp $
+# $Id: complete.cmd,v 1.3 2006/03/19 11:09:40 rockyb Exp $
 #
 print "*** Testing complete***"
 complete d
 complete fi
+complete set a
+complete  show a
+complete set an
+complete set args
+complete show args1
+complete bogus command
+complete info f
