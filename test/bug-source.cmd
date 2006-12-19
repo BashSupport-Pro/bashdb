d08fa7300dfcc337c1648601b427b4bc640e87b7
rockyb <rockyb>
2006-12-19 06:44:25 +0000

    Files names coming from function line-specificiations should be read in. Debian Bug#403306:

---
+++ test/bug-source.cmd
@@ -0,0 +1,10 @@
+# Test to see that we read in files that mentioned in breakpoints
+# but we don't step into.
+continue 34
+# It is important to "next" rather than "step"
+next
+# The following breakpoint should cause 
+# a file to get read in.
+break sourced_fn
+info files
+quit
