e3c5bd3d1995fd39342f52349d226d82dc0cf2e5
rockyb <rockyb>
2007-02-11 23:06:41 +0000

    Set PS4 to a better value and make sure to restore it when going back into the user program. Also restore on quit. Likewise for other variables.

--- test/bugIFS.cmd
+++ test/bugIFS.cmd
@@ -4,5 +4,11 @@ set trace-co on
 ### Going to the location where IFS should be reset in the code...
 continue 5
 e _Dbg_print_source_line 5
+step
+## Make sure IFS in an eval is the same as what we just set.
+eval declare -p IFS
+step 2
+## Make sure PS4 in an eval is the same as what we just set.
+eval declare -p PS4
 quit
 
