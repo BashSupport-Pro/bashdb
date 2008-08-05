2fa1fec7861062650d772bd3711c15dcec8a0215
rockyb <rockyb>
2008-08-05 11:33:55 +0000

    Add copyleft.

--- builtin/set0.c
+++ builtin/set0.c
@@ -1,5 +1,22 @@
 /*   
-  Set $0, the program name.
+# set0.c - Set $0, the program name.
+#
+#   Copyright (C) 2008 Rocky Bernstein  rocky@gnu.org
+#
+#   Bash is free software; you can redistribute it and/or modify it under
+#   the terms of the GNU General Public License as published by the Free
+#   Software Foundation; either version 2, or (at your option) any later
+#   version.
+#
+#   Bash is distributed in the hope that it will be useful, but WITHOUT ANY
+#   WARRANTY; without even the implied warranty of MERCHANTABILITY or
+#   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+#   for more details.
+#   
+#   You should have received a copy of the GNU General Public License along
+#   with Bash; see the file COPYING.  If not, write to the Free Software
+#   Foundation, 59 Temple Place, Suite 330, Boston, MA 02111 USA.
+#  
 
   To install after compiling:
      cd *this directory* 
@@ -11,7 +28,7 @@
 #include <stdio.h>
 
 char *set0_doc[] = {
-  "Set $0.",
+  "Set $0, the program name.",
   NULL
 };
 
