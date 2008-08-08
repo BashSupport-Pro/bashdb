536765706809f7610a4e5ee2c30351ecd38b1c1f
rockyb <rockyb>
2008-08-08 09:14:52 +0000

    Clean up and shorten bugIFS.sh and the testing framework a little. More later

--- test/bugIFS.cmd
+++ test/bugIFS.cmd
@@ -1,14 +1,7 @@
 set trace-co on
 # Debugger test of an old IFS bug
 #
-### Going to the location where IFS should be reset in the code...
-continue 5
-e _Dbg_print_source_line 5
 step
-## Make sure IFS in an eval is the same as what we just set.
-eval declare -p IFS
-step 2
 ## Make sure PS4 in an eval is the same as what we just set.
-eval declare -p PS4
+p "+$IFS+"
 quit
-
