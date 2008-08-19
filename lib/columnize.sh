6b31d35f6b560a997078ab21a3983fb38e8bbc1e
rockyb <rockyb>
2008-08-19 11:50:38 +0000

    Add save/restore unit test from kshdb (and zshdb).

--- lib/columnize.sh
+++ lib/columnize.sh
@@ -1,18 +1,18 @@
 # -*- shell-script -*-
 #   Copyright (C) 2008 Rocky Bernstein rocky@gnu.org
 #
-#   kshdb is free software; you can redistribute it and/or modify it under
+#   bashdb is free software; you can redistribute it and/or modify it under
 #   the terms of the GNU General Public License as published by the Free
 #   Software Foundation; either version 2, or (at your option) any later
 #   version.
 #
-#   kshdb is distributed in the hope that it will be useful, but WITHOUT ANY
+#   bashdb is distributed in the hope that it will be useful, but WITHOUT ANY
 #   WARRANTY; without even the implied warranty of MERCHANTABILITY or
 #   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
 #   for more details.
 #   
 #   You should have received a copy of the GNU General Public License along
-#   with kshdb; see the file COPYING.  If not, write to the Free Software
+#   with bashdb; see the file COPYING.  If not, write to the Free Software
 #   Foundation, 59 Temple Place, Suite 330, Boston, MA 02111 USA.
 #
 #  Code ported from my Ruby code which is in turn ported from a routine
