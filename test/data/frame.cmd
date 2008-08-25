e009a1095ade73c247139fd719ab1f99d8b8dfc4
rockyb <rockyb>
2008-08-25 23:33:32 +0000

    Make test-frame less subject to changes in bashdb code by limiting number of frames in backtraces.

--- test/data/frame.cmd
+++ test/data/frame.cmd
@@ -2,37 +2,38 @@ set trace-commands on
 # Test of frame commands
 # We also try all of the various where/backtrace variants
 # Do we give a valid stack listing initially?
-where
+# Let's start with a couple of stack entries
+continue hanoi
+where 2
 # How about after a frame command? 
 frame 0
-bt
-where
+bt 2
 # How about after moving?
 u
-where
+where 2
 down
-where
+where 2
 # Try moving past the end
 down
-where
-up 2
-bt
+where 2
+up 3
+bt 2
 # Try some negative numbers
 # should be the same as up
 down -1
-T
-# Should to to least recent frame
-frame -1
+T 2
+# Should go to next-to-least-recent frame
+frame -2
 where
 # Let's add another stack entry
 continue hanoi
-where
-# Again least recent stack entry
-frame -1
-where
+where 3
+# Again, next-to-least recent stack entry
+frame -2
+where 3
 # Most recent stack entry
 frame +0
-backtrace
+backtrace 3
 up 2
 quit
 
