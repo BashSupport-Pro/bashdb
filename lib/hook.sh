794d661f76dd8160855eba57a34cbe44d3ab441e
rocky <rocky@gnu.org>
2010-06-18 00:52:17 -0400

    More work on fixing breakpoint handling. I think we are a little closer (perhaps 19 vs 22 failing now).

--- lib/hook.sh
+++ lib/hook.sh
@@ -43,164 +43,188 @@ typeset -i _Dbg_restore_debug_trap=1
 
 _Dbg_debug_trap_handler() {
 
-  ### The below is also copied below in _Dbg_sig_handler...
-  ### Should put common stuff into a function.
+    ### The below is also copied below in _Dbg_sig_handler...
+    ### Should put common stuff into a function.
+    
+    # Consider putting the following line(s) in a routine.
+    # Ditto for the restore environment
+    typeset -i _Dbg_debugged_exit_code=$?
+    _Dbg_old_set_opts=$-
+    shopt -s extdebug
+    
+    # Turn off line and variable trace listing if were not in our own debug
+    # mode, and set our own PS4 for debugging inside the debugger
+    (( !_Dbg_debug_debugger )) && set +x +v +u
+    
+    # If we are in our own routines -- these start with _bashdb -- then
+    # return.
+    if [[ ${FUNCNAME[1]} == _Dbg_* ]] && ((  !_Dbg_debug_debugger )); then
+	_Dbg_set_to_return_from_debugger 0
+	return 0
+    fi
 
-  # Consider putting the following line(s) in a routine.
-  # Ditto for the restore environment
-  typeset -i _Dbg_debugged_exit_code=$?
-  _Dbg_old_set_opts=$-
-  shopt -s extdebug
-
-  # Turn off line and variable trace listing if were not in our own debug
-  # mode, and set our own PS4 for debugging inside the debugger
-  (( !_Dbg_debug_debugger )) && set +x +v +u
-
-  # If we are in our own routines -- these start with _bashdb -- then
-  # return.
-  if [[ ${FUNCNAME[1]} == _Dbg_* ]] && ((  !_Dbg_debug_debugger )); then
-    _Dbg_set_to_return_from_debugger 0
-    return 0
-  fi
-
-  _Dbg_set_debugger_entry
-
-  typeset -i _Dbg_rc=0
-
-  # Shift off "RETURN";  we do not need that any more.
-  shift 
-
-  _Dbg_bash_command=$1
-  shift
-
-  # Save values of $1 $2 $3 when debugged program was stopped
-  # We use the loop below rather than _Dbg_set_args="(@)" because
-  # we want to preserve embedded blanks in the arguments.
-  typeset -i _Dbg_n=${#@}
-  typeset -i _Dbg_i
-  typeset -i _Dbg_arg_max=${#_Dbg_arg[@]}
-
-  # If there has been a shift since the last time we entered,
-  # it is possible that _Dbg_arg will contain too many values.
-  # So remove those that have disappeared.
-  for (( _Dbg_i=_Dbg_arg_max; _Dbg_i > _Dbg_n ; _Dbg_i-- )) ; do
-      unset _Dbg_arg[$_Dbg_i]
-  done
- 
-  # Populate _Dbg_arg with $1, $2, etc.
-  for (( _Dbg_i=1 ; _Dbg_n > 0; _Dbg_n-- )) ; do
-    _Dbg_arg[$_Dbg_i]=$1
-    ((_Dbg_i++))
+    # Sets _Dbg_frame_last_lineno and _Dbg_frame_last_filename among
+    # other things.
+    _Dbg_set_debugger_entry
+    
+    typeset -i _Dbg_rc=0
+    
+    # Shift off "RETURN";  we do not need that any more.
+    shift 
+    
+    _Dbg_bash_command=$1
     shift
-  done
-  unset _Dbg_arg[0]       # Get rid of line number; makes array count
-                          # correct; also listing all _Dbg_arg works
-                          # like $*.
-
-  # if in step mode, decrement counter
-  if ((_Dbg_step_ignore > 0)) ; then 
-    ((_Dbg_step_ignore--))
-    _Dbg_write_journal "_Dbg_step_ignore=$_Dbg_step_ignore"
-  fi
-
-  # look for watchpoints.
-  typeset -i _Dbg_i
-  for (( _Dbg_i=0; _Dbg_i < _Dbg_watch_max ; _Dbg_i++ )) ; do
-    if [ -n "${_Dbg_watch_exp[$_Dbg_i]}" ] \
-      && [[ ${_Dbg_watch_enable[_Dbg_i]} != 0 ]] ; then
-      typeset new_val=`_Dbg_get_watch_exp_eval $_Dbg_i`
-      typeset old_val=${_Dbg_watch_val[$_Dbg_i]}
-      if [[ $old_val != $new_val ]] ; then
-	((_Dbg_watch_count[_Dbg_i]++))
-	_Dbg_msg "Watchpoint $_Dbg_i: ${_Dbg_watch_exp[$_Dbg_i]} changed:"
-	_Dbg_msg "  old value: '$old_val'"
-	_Dbg_msg "  new value: '$new_val'"
-	_Dbg_print_location_and_command
-	_Dbg_watch_val[$_Dbg_i]=$new_val
-	_Dbg_hook_enter_debugger "on a watch trigger"
-	return $_Dbg_rc
-      fi
-    fi
-  done
-
-  # We are restoring IFS here! probably in dbg-set-dvars.inc
-  # # Run applicable action statement
-  # typeset entries=_Dbg_action_file2linenos[$_Dbg_frame_last_filename]
-  # if [[ $entries != "" ]]  ; then
-  #   typeset -i _Dbg_i
-  #   for _Dbg_i in $entries ; do
-  #     if [[ ${_Dbg_action_enable[_Dbg_i]} != 0 ]] ; then
-  # 	. ${_Dbg_libdir}/dbg-set-d-vars.inc
-  # 	eval "${_Dbg_action_stmt[$_Dbg_i]}"
-  #     fi
-  #   done
-  # fi
-
-  # check if breakpoint reached
-  typeset entries=
-  entries=${_Dbg_brkpt_file2linenos[$_Dbg_frame_last_filename]}
-  if [[ $entries != "" ]]  ; then
+    
+    # Save values of $1 $2 $3 when debugged program was stopped
+    # We use the loop below rather than _Dbg_set_args="(@)" because
+    # we want to preserve embedded blanks in the arguments.
+    typeset -i _Dbg_n=${#@}
     typeset -i _Dbg_i
-    for _Dbg_i in $entries ; do
-      if [[ ${_Dbg_brkpt_enable[_Dbg_i]:0} != 0 ]] ; then
-	typeset -i cond
-	. ${_Dbg_libdir}/dbg-set-d-vars.inc
-	cond=${_Dbg_brkpt_cond[$_Dbg_i]:0}
-	if [[ $cond != 0 ]] ; then
-	  ((_Dbg_brkpt_count[_Dbg_i]++))
-	  if [[ ${_Dbg_brkpt_onetime[_Dbg_i]:0} == 1 ]] ; then
-	    _Dbg_stop_reason='at a breakpoint that has since been deleted'
-	    _Dbg_delete_brkpt_entry $_Dbg_i
-	  else
-	    _Dbg_brkpt_num=$_Dbg_i
-	     _Dbg_stop_reason="at breakpoint $_Dbg_brkpt_num"
-	    _Dbg_msg \
-              "Breakpoint $_Dbg_i hit (${_Dbg_brkpt_count[$_Dbg_i]} times)."
-	  fi
-	  # We're sneaky and check commands_end because start could 
-	  # legitimately be 0.
-	  if (( ${_Dbg_brkpt_commands_end[$_Dbg_i]} )) ; then
-	      # Run any commands associated with this breakpoint
-	      _Dbg_bp_commands $_Dbg_i
-	  fi
-	  _Dbg_hook_enter_debugger _Dbg_stop_reason
-	  return $_Dbg_rc
-	fi
-      fi
-
+    typeset -i _Dbg_arg_max=${#_Dbg_arg[@]}
+    
+    # If there has been a shift since the last time we entered,
+    # it is possible that _Dbg_arg will contain too many values.
+    # So remove those that have disappeared.
+    for (( _Dbg_i=_Dbg_arg_max; _Dbg_i > _Dbg_n ; _Dbg_i-- )) ; do
+	unset _Dbg_arg[$_Dbg_i]
     done
-  fi
+    
+    # Populate _Dbg_arg with $1, $2, etc.
+    for (( _Dbg_i=1 ; _Dbg_n > 0; _Dbg_n-- )) ; do
+	_Dbg_arg[$_Dbg_i]=$1
+	((_Dbg_i++))
+	shift
+    done
+    unset _Dbg_arg[0]       # Get rid of line number; makes array count
+                            # correct; also listing all _Dbg_arg works
+                            # like $*.
 
-  # Check if step mode and number steps to ignore.
-  if ((_Dbg_step_ignore == 0)); then
-
-      if ((_Dbg_step_force)) ; then
-	  if (( $_Dbg_last_lineno == $_Dbg_frame_last_lineno )) \
-	      && [[ $_Dbg_last_source_file == $_Dbg_frame_last_filename ]] ; then 
-	      _Dbg_set_to_return_from_debugger 1
-	      return $_Dbg_rc
-	  fi
-      fi
-
-      _Dbg_hook_enter_debugger 'after being stepped'
-      return $_Dbg_rc
-  elif (( ${#FUNCNAME[@]} == _Dbg_return_level )) ; then
-      # here because a trap RETURN
-      _Dbg_return_level=0
-      _Dbg_hook_enter_debugger 'on a return'
-      return $_Dbg_rc
-  elif (( -1 == _Dbg_return_level )) ; then
-      # here because we are fielding a signal.
-      _Dbg_hook_enter_debugger 'on fielding signal'
-      return $_Dbg_rc
-  elif ((_Dbg_linetrace==1)) ; then 
-    if ((_Dbg_linetrace_delay)) ; then
-	sleep $_Dbg_linetrace_delay
+    # if in step mode, decrement counter
+    if ((_Dbg_step_ignore > 0)) ; then 
+	((_Dbg_step_ignore--))
+	_Dbg_write_journal "_Dbg_step_ignore=$_Dbg_step_ignore"
     fi
-    _Dbg_print_linetrace
-  fi
-  _Dbg_set_to_return_from_debugger 1
-  return $_Dbg_inside_skip
+    
+    # look for watchpoints.
+    typeset -i _Dbg_i
+    for (( _Dbg_i=0; _Dbg_i < _Dbg_watch_max ; _Dbg_i++ )) ; do
+	if [ -n "${_Dbg_watch_exp[$_Dbg_i]}" ] \
+	    && [[ ${_Dbg_watch_enable[_Dbg_i]} != 0 ]] ; then
+	    typeset new_val=`_Dbg_get_watch_exp_eval $_Dbg_i`
+	    typeset old_val=${_Dbg_watch_val[$_Dbg_i]}
+	    if [[ $old_val != $new_val ]] ; then
+		((_Dbg_watch_count[_Dbg_i]++))
+		_Dbg_msg "Watchpoint $_Dbg_i: ${_Dbg_watch_exp[$_Dbg_i]} changed:"
+		_Dbg_msg "  old value: '$old_val'"
+		_Dbg_msg "  new value: '$new_val'"
+		_Dbg_print_location_and_command
+		_Dbg_watch_val[$_Dbg_i]=$new_val
+		_Dbg_hook_enter_debugger "on a watch trigger"
+		return $_Dbg_rc
+	    fi
+	fi
+    done
+    
+    # Run applicable action statement
+    typeset entries=_Dbg_action_file2linenos[$_Dbg_frame_last_filename]
+    if [[ $entries != "" ]]  ; then
+	typeset -i _Dbg_i
+	for _Dbg_i in $entries ; do
+	    if [[ ${_Dbg_action_enable[_Dbg_i]} != 0 ]] ; then
+  		. ${_Dbg_libdir}/dbg-set-d-vars.inc
+  		eval "${_Dbg_action_stmt[$_Dbg_i]}"
+		# We've reset some variables like IFS and PS4 to make eval look
+		# like they were before debugger entry - so reset them now.
+		_Dbg_set_debugger_internal
+	    fi
+	done
+    fi
+    
+    # check if breakpoint reached
+    if ((_Dbg_brkpt_count > 0)) ; then 
+	if _Dbg_hook_breakpoint_hit ; then 
+	    if ((_Dbg_step_force)) ; then
+		typeset _Dbg_frame_previous_file="$_Dbg_frame_last_filename"
+		typeset -i _Dbg_frame_previous_lineno="$_Dbg_frame_last_lineno"
+	    fi
+	    ## FIXME: should probably add this (from zshdb):
+	    ## _Dbg_frame_save_frames 1
+	    ((_Dbg_brkpt_counts[_Dbg_brkpt_num]++))
+	    _Dbg_msg \
+              "Breakpoint $_Dbg_brkpt_num hit (${_Dbg_brkpt_counts[_Dbg_brkpt_num]} times)."
+	    if (( ${_Dbg_brkpt_onetime[_Dbg_brkpt_num]} == 1 )) ; then
+		_Dbg_stop_reason='at a breakpoint that has since been deleted'
+		_Dbg_delete_brkpt_entry $_Dbg_brkpt_num
+	    else
+		_Dbg_stop_reason="at breakpoint $_Dbg_brkpt_num"
+	    fi
+	    _Dbg_hook_enter_debugger "$_Dbg_stop_reason"
+	    return $?
+	fi
+    fi
+    
+
+    # Check if step mode and number steps to ignore.
+    if ((_Dbg_step_ignore == 0)); then
+	
+	if ((_Dbg_step_force)) ; then
+	    if (( $_Dbg_last_lineno == $_Dbg_frame_last_lineno )) \
+		&& [[ $_Dbg_last_source_file == $_Dbg_frame_last_filename ]] ; then 
+		_Dbg_set_to_return_from_debugger 1
+		return $_Dbg_rc
+	    fi
+	fi
+	
+	_Dbg_hook_enter_debugger 'after being stepped'
+	return $_Dbg_rc
+    elif (( ${#FUNCNAME[@]} == _Dbg_return_level )) ; then
+	# here because a trap RETURN
+	_Dbg_return_level=0
+	_Dbg_hook_enter_debugger 'on a return'
+	return $_Dbg_rc
+    elif (( -1 == _Dbg_return_level )) ; then
+	# here because we are fielding a signal.
+	_Dbg_hook_enter_debugger 'on fielding signal'
+	return $_Dbg_rc
+    elif ((_Dbg_linetrace==1)) ; then 
+	if ((_Dbg_linetrace_delay)) ; then
+	    sleep $_Dbg_linetrace_delay
+	fi
+	_Dbg_print_linetrace
+    fi
+    _Dbg_set_to_return_from_debugger 1
+    return $_Dbg_inside_skip
+}
+
+# Return 0 if we are at a breakpoint position or 1 if not.
+# Sets _Dbg_brkpt_num to the breakpoint number found.
+_Dbg_hook_breakpoint_hit() {
+    typeset full_filenaname
+    filename="$_Dbg_frame_last_filename"
+    lineno=$_Dbg_frame_last_lineno
+    full_filename=$(_Dbg_is_file $filename)
+    [[ -z $full_filename ]] && full_filename="$filename"
+    if [[ -r $full_filename ]] ; then 
+	_Dbg_file2canonic[$filename]="$full_filename"
+    fi
+    # FIXME: combine with _Dbg_unset_brkpt
+    typeset -a linenos
+    [[ -z ${_Dbg_brkpt_file2linenos[$full_filename]} ]] && return 1
+    eval "linenos=(${_Dbg_brkpt_file2linenos[$full_filename]})"
+    typeset -a brkpt_nos
+    eval "brkpt_nos=(${_Dbg_brkpt_file2brkpt[$full_filename]})"
+    typeset -i i
+    # Check breakpoints within full_filename
+    for ((i=0; i < ${#linenos[@]}; i++)); do 
+	if (( linenos[i] == lineno )) ; then
+	    # Got a match, but is the breakpoint enabled? 
+	    (( _Dbg_brkpt_num = brkpt_nos[i] ))
+	    if ((_Dbg_brkpt_enable[_Dbg_brkpt_num] )) ; then
+		return 0
+	    fi
+	fi
+    done
+    return 1
 }
 
 # Go into the command loop
