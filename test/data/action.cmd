40e07f454ed477402b872d20fe2e91c21ea0b87e
rocky <rocky@gnu.org>
2010-06-20 22:03:57 -0400

    Automake automation

--- test/data/action.cmd
+++ test/data/action.cmd
@@ -1,10 +1,13 @@
 set trace-commands on
+set basename on
 # Debugger test of action command
 #
-# Try a simple action breakpoint...
+# Try a simple action action...
+a
 a 23 x=60
 L
+a
 cont 24
 print "value of x is now $x"
 quit
-
+set trace-commands on
