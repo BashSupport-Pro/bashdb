d14d0670fe3425c7b6ce33e66379daa745203c38
R. Bernstein <rocky@gnu.org>
2010-06-21 13:02:53 -0400

    Use confirm routine to delete all actions. Better synch with zshdb too. Proper count tracing of actions fixed too.

--- test/data/action.cmd
+++ test/data/action.cmd
@@ -2,12 +2,14 @@ set trace-commands on
 set basename on
 # Debugger test of action command
 #
-# Try a simple action action...
+# Show actions
 a
+# Delete actions when there are none
+A
+# Try a simple action action...
 a 23 x=60
 L
 a
 cont 24
 print "value of x is now $x"
 quit
-set trace-commands on
