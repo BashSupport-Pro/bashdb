bb6991667e4d9dd7eba6017b2f462d3954360394
rocky <rocky@gnu.org>
2010-10-25 14:47:35 -0400

    Add test-delete integration test from zshdb.

---
+++ test/example/stepping.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+# For testing break, delete, step, step+ step-, default step and set force.
+for ((i=0; i<3; i++)) do echo 1st loop $i ; done
+for ((i=0; i<3; i++)) do echo 2nd loop $i ; done
+for ((i=0; i<3; i++)) do echo 3rd loop $i ; done
+for ((i=0; i<3; i++)) do echo 4th loop $i ; done
+for ((i=0; i<3; i++)) do echo 5th loop $i ; done
+for ((i=0; i<3; i++)) do echo 6th loop $i ; done
