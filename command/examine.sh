cd8525ba0c1d994f298f81fbcfe1d9a9855cc2d2
rocky <rocky@gnu.org>
2010-12-24 13:23:24 -0500

    Remove one more erroneous use of _Dbgr_continue_rc

--- command/examine.sh
+++ command/examine.sh
@@ -41,7 +41,7 @@ function _Dbg_do_examine {
     else 
 	typeset -i _Dbg_rc
 	eval let _Dbg_result=$_Dbg_expr 2>/dev/null; _Dbg_rc=$?
-	if (( $_Dbg_continue_rc != 0 )) ; then
+	if (( $_Dbg_rc != 0 )) ; then
 	    _Dbg_do_print "$_Dbg_expr"
 	else
 	    _Dbg_msg "$_Dbg_result"
