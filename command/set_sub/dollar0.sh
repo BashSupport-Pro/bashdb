8af512b5dd01aebc45cbb46c68f13de0aa1d9e19
rocky <rocky@gnu.org>
2011-05-03 14:19:17 -0400

    Only add "set dollar0" if "set0" builtin loads.

--- command/set_sub/dollar0.sh
+++ command/set_sub/dollar0.sh
@@ -18,17 +18,25 @@
 #   the Free Software Foundation, 59 Temple Place, Suite 330, Boston,
 #   MA 02111 USA.
 
-_Dbg_help_add_sub set dollar0 \
-'Set $0.
-Follow this command with any number of args, to be passed to the program.' 1
+# Load set0 if possible.
+if [[ -f $_Dbg_libdir/builtin/set0 ]] ; then
+    enable -f $_Dbg_libdir/builtin/set0 set0
+fi
 
-_Dbg_do_set_dollar0() {
-    # We use the loop below rather than _Dbg_set_args="(@)" because
-    # we want to preserve embedded blanks in the arguments.
-    if enable -a set0 2>/dev/null ; then
-	set0 "$1"
-    else
-	_Dbg_errmsg "Can't do becasue set0 module is not loaded."
-    fi
-    return 0
-}
+# If it was set0 loaded, then we can add a debugger command "set dollar0"
+if enable -a set0 2>/dev/null ; then
+    _Dbg_help_add_sub set dollar0 \
+	'Set $0.
+Follow this command with any number of args, to be passed to the program.' 1
+    
+    _Dbg_do_set_dollar0() {
+	# We use the loop below rather than _Dbg_set_args="(@)" because
+	# we want to preserve embedded blanks in the arguments.
+	if enable -a set0 2>/dev/null ; then
+	    set0 "$1"
+	else
+	    _Dbg_errmsg "Can't do becasue set0 module is not loaded."
+	fi
+	return 0
+    }
+fi
