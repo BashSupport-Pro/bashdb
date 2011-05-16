b67149b35bf9e3c6a74bc4a6b0770a84759be4a4
rocky <rocky@gnu.org>
2011-05-16 06:11:31 -0400

    Add test unit helper routine and start long process of DRYing code.

---
+++ test/unit/helper.sh
@@ -0,0 +1,7 @@
+PS4='-(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]} - [${SHLVL},${BASH_SUBSHELL}, $?]
+'
+_Dbg_libdir=$abs_top_srcdir
+shunit_file=${abs_top_srcdir}test/unit/shunit2
+
+# Don't need to show banner
+set -- '-q'  
